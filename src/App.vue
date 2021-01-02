f7606fedf7c3b04d1639f9ed50502cbcd2825fc4
“Qwy-77” <992025847@qq.com>
2021-01-02 18:57:00 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-   
+  <router-view></router-view>
+  <Main-tab-bar></Main-tab-bar>
+  </div>
 </template>
 
 <script>
-
+import MainTabBar from "./components/content/mainTabbar/MainTabBar"
 
 export default {
   name: 'App',
   components: {
-   
+   MainTabBar
   }
 }
 </script>
 
 <style>
-
+ @import 'assets/css/base.css';
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,14 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        'components': '@/components',
-        'content': 'components/content',
-        'common': 'components/common',
-        'assets': '@/assets',
-        'network': '@/network',
-        'views': '@/views',
+        components: "@/components",
+        content: "components/content",
+        common: "components/common",
+        // common: "@/common",
+        assets: "@/assets",
+        network: "@/network",
+        views: "@/views"
       }
     }
   }
-}
+};
